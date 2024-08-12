d95731bf8565cb694e68610d55cbbda270c21b5c
mamun10fx <159600247+mamun10fx@users.noreply.github.com>
2024-08-12 12:52:44 +0600

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -17,8 +17,8 @@ module.exports = {
   },
 
   onStart: async function ({ args, message, event, api, usersData }) {
-    const permission = ["61562362827346"];
-  if (!permission.includes(event.senderID)) {
+    const permission = [];
+  if (permission.includes(event.senderID)) {
     api.sendMessage("Only Gab Yu can use this command!", event.threadID, event.messageID);
     return;
   }
